c0b2139e7cde38f67c94e6a9c79ab3869936cd02
xierfloat <164899869+dfwgj@users.noreply.github.com>
2024-07-15 22:54:08 +0800

    2.002

--- src/dao/comtBodyDao.js
+++ src/dao/comtBodyDao.js
@@ -12,3 +12,27 @@ exports.getComtBody = async (cbId) => {
     const sqlParams = [cbId];
     return await db.query(sql, sqlParams);
 };
+exports.createComtBody = async (cbId, cbimg, cbtext, cbtitle) => {
+    const sql = `
+        INSERT 
+        INTO 
+        e_comt_body (cb_id, cb_img, cb_text, cb_title)
+        VALUES (?, ?, ?, ?)
+    `;
+    const sqlParams = [cbId, cbimg, cbtext, cbtitle];
+    return await db.query(sql, sqlParams);
+};
+
+exports.modifyComtBody = async (cbId, cbimg, cbtext, cbtitle) => {
+    const sql = `
+        UPDATE e_comt_body
+        SET 
+            cb_img = ?,
+            cb_text = ?,
+            cb_title = ?
+        WHERE
+            cb_id = ?
+    `;
+    const sqlParams = [cbimg, cbtext, cbtitle, cbId];
+    return await db.query(sql, sqlParams);
+};
--- src/service/comtBodyService.js
+++ src/service/comtBodyService.js
@@ -4,3 +4,13 @@ exports.getComtBody = async (cbId) => {
     const comtBody = await comtBodyDao.getComtBody(cbId);
     return comtBody;
 };
+
+exports.createComtBody = async (cbId, cbimg, cbtext, cbtitle) => {
+    const comtBody = await comtBodyDao.createComtBody(cbId, cbimg, cbtext, cbtitle);
+    return comtBody;
+};
+
+exports.modifyComtBody = async (cbId, cbimg, cbtext, cbtitle) => {
+    const comtBody = await comtBodyDao.modifyComtBody(cbId, cbimg, cbtext, cbtitle);
+    return comtBody;
+};
